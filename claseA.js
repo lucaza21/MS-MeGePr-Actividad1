ed4802328d0a64e328f565a871ddd3704795b9b5
lucaza21 <luis.carrasquilla.z@gmail.com>
2023-06-18 21:55:21 -0500

    fixed issues - closes #2, closes #3

---
+++ claseA.js
@@ -0,0 +1,11 @@
+# se crea la clase A
+class ClaseA {
+        constructor(foo,bar){
+                this.foo = "foo";
+                this.bar = "bar";
+                }
+        displayInfo( {
+                console.log(`esto es ${this.foo} y eso es ${bar}`);
+                }
+        }
+
