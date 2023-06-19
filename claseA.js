9ee3efa9991f494c5798d4545230da6020c5e1ac
lucaza21 <luis.carrasquilla.z@gmail.com>
2023-06-18 21:34:33 -0500

    fixed issues - closes #1, closes #2, closes #3

---
+++ claseA.js
@@ -0,0 +1,9 @@
+class ClaseA {
+        constructor(foo,bar){
+                this.foo = "foo";
+                this.bar = "bar";
+                }
+        displayInfo( {
+                console.log(`esto es ${this.foo} y eso es ${bar}, chevere`);
+                }
+        }
