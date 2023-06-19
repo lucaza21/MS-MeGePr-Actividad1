91a35e417093df6b78817b670578c772940efec2
lucaza21 <luis.carrasquilla.z@gmail.com>
2023-06-18 22:41:16 -0500

    fixed issues - closes #5, closes #7

---
+++ claseB.js
@@ -0,0 +1,12 @@
+# se crea la clase B en feature-02
+
+class ClaseB {
+        constructor(foo,bar){
+                this.foo = "foo";
+                this.bar = "bar";
+                }
+        displayInfo( {
+                console.log(`esto es ${this.foo} y eso es ${bar}, B`);
+                }
+        }
+
